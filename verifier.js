b59fe52fdf3644bf9a65b41ffabff99cce7a9f42
felipe <felipemoreno148@gmail.com>
2024-11-17 09:20:46 -0300

    refatoring done

--- verifier.js
+++ verifier.js
@@ -1,33 +1,42 @@
 import { input } from "@inquirer/prompts";
 
 async function passwordVerifier() {
-
-  let password = await input({message: 'Digite sua senha'})
+  let password = await input({ message: "Digite sua senha" });
 
   if (password.length == 0) {
-    console.log(' Digite uma senha. ');
+    console.log(" Digite uma senha. ");
+    return passwordVerifier();
   }
   if (!password.match(/^.*[0-9].*$/)) {
-    console.log(' A senha deve ter pelo menos um número. ');
+    console.log(" A senha deve ter pelo menos um número. ");
+    return passwordVerifier();
   }
   if (password.length < 8) {
-    console.log(' A senha deve ter pelo menos 8 caracteres. ');
+    console.log(" A senha deve ter pelo menos 8 caracteres. ");
+    return passwordVerifier();
   }
   if (!password.match(/^.*[a-z].*$/)) {
-    console.log(' A senha deve ter pelo menos uma letra minúscula. ');
+    console.log(" A senha deve ter pelo menos uma letra minúscula. ");
+    return passwordVerifier();
   }
   if (!password.match(/^.*[A-Z].*$/)) {
-    console.log(' A senha deve ter pelo menos uma letra maiúscula. ') ;
+    console.log(" A senha deve ter pelo menos uma letra maiúscula. ");
+    return passwordVerifier();
   }
   if (!password.match(/[@#$%^&*()<>';:=!^-]/g)) {
-    console.log(' A senha deve ter pelo menos um caractere especial. ') ;
+    console.log(" A senha deve ter pelo menos um caractere especial. ");
+    return passwordVerifier();
   }
 
-  password = password || 'Sua senha é segura.'
+  password = password || "Sua senha é segura.";
+  console.log("sua senha é segura.");
 }
 
+async function start() {
+  passwordVerifier();
+}
 
-export default passwordVerifier;
+start();
 
 /*
 ' A senha deve ter pelo menos uma letra minúscula. '
